6537541818ed0517e5fe7ef46d887522e2137ebc
Aniruddha Pai <agpai2@illinois.edu>
2021-04-26 10:38:31 -0500

    Added a new tab

--- demoproject/frontend/src/App.js
+++ demoproject/frontend/src/App.js
@@ -3,33 +3,35 @@ import Tabs from "./components/Tabs";
 // import React, { useState, useEffect } from "react";
 
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import Home from './Home'
-import Song from './Song';
-import About from './About'
-import Navbar from './Navbar';
 
+import Home from './js/Home'
+import Song from './js/Song'
+import About from './js/About'
+import Navbar from './js/Navbar'
+import Contact from './js/Contact'
 
 function App() {
-
   return (
     <Router>
       <div className="App">
         <Navbar />
-          <div className="content">
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
 
-              <Route path="/Song">
-                <Song />
-              </Route>
+            <Route path="/Song">
+              <Song />
+            </Route>
 
             <Route path="/About">
               <About />
             </Route>
-            </Switch>
-          </div>
+
+            <Route path="/Contact">
+              <Contact />
+            </Route>
+          </Switch>
       </div>
     </Router>
 
